480cb970d828b15c219fa72f2f1ba93e2d67790b
ajian2002 <ajian020515@163.com>
2021-05-22 16:22:18 +0800

    fix bugs

--- install2.sh
+++ install2.sh
@@ -62,31 +62,6 @@ else
     exit -1
 fi
 
-read -p "hostname:" userhostname
-echo $userhostname >>/etc/hostname
-if [ $? -eq 0 ]; then
-    echo "change hostname success "
-else
-    echo "change hostname failed "
-    exit -1
-fi
-
-echo "root passed:"
-passwd
-
-echo "%wheel ALL=(ALL) ALL " >>/etc/sudoers
-
-read -p "Normal user " username
-useradd -m -G wheel $username
-echo "$username passwd"
-passwd $username
-if [ $? -eq 0 ]; then
-    echo "user $username add success "
-else
-    echo "user $username add failed "
-    exit -1
-fi
-
 echo "fonts installing  (Google Noto Fonts 字体,思源黑体, 思源宋体,更纱黑体)"
  pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-sarasa-gothic adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts  --noconfirm
 if [ $? -eq 0 ]; then
@@ -106,18 +81,22 @@ else
     exit -1
 fi
 
-echo "pinyin installing"
- pacman -S fcitx fcitx-im fcitx-libpinyin --noconfirm
-echo -e "GTK_IM_MODULE DEFAULT=fcitx\nQT_IM_MODULE  DEFAULT=fcitx\nXMODIFIERS    DEFAULT=@im=fcitx " >/home/$username/.pam_environment
+read -p "hostname:" userhostname
+echo $userhostname >>/etc/hostname
 if [ $? -eq 0 ]; then
-    echo "pinyinsuccess"
+    echo "change hostname success "
 else
-    echo "pinyinfailed"
+    echo "change hostname failed "
     exit -1
 fi
 
+echo "root passed:"
+passwd
+
+echo "%wheel ALL=(ALL) ALL " >>/etc/sudoers
+
 echo "blue tooth installing"
- pacman -S bluez bluez-utils pulseaudio-bluetooth  --noconfirm
+pacman -S bluez bluez-utils pulseaudio-bluetooth  --noconfirm
 modprobe btusb
 systemctl enable bluetooth.service
 if [ $? -eq 0 ]; then
@@ -127,31 +106,6 @@ else
     exit -1
 fi
 
-echo "$usernameuser language configing"
-echo -e "LANG=zh_CN.UTF-8\nLC_CTYPE=\"zh_CN.UTF-8\"\nLC_NUMERIC=\"zh_CN.UTF-8\"\nLC_TIME=\"zh_CN.UTF-8\"\n
-LC_COLLATE=\"zh_CN.UTF-8\"\nLC_MONETARY=\"zh_CN.UTF-8\"\nLC_MESSAGES=\"zh_CN.UTF-8\"\nLC_PAPER=\"zh_CN.UTF-8\"\n
-LC_NAME=\"zh_CN.UTF-8\"\nLC_ADDRESS=\"zh_CN.UTF-8\"\nLC_TELEPHONE=\"zh_CN.UTF-8\"\nLC_MEASUREMENT=\"zh_CN.UTF-8\"\n
-LC_IDENTIFICATION=\"zh_CN.UTF-8\"\nLC_ALL= " >/home/$username/.config/locale.conf
-if [ $? -eq 0 ]; then
-    echo "$username user languageconfig success"
-else
-    echo "$username user languageconfig failed"
-    exit -1
-fi
-
-echo "zsh configing"
- pacman -S zsh oh-my-zsh-git zsh-syntax-highlighting zsh-autosuggestions autojump  --noconfirm
-chsh -s /bin/zsh $username
-ln -s /usr/share/zsh/plugins/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/
-ln -s /usr/share/zsh/plugins/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/
-cp /usr/share/oh-my-zsh/zshrc /home/$username/.zshrc
-sed 's/plugins=(git)/plugins=(autojump git zsh-syntax-highlighting zsh-autosuggestions)' /home/$username/.zshrc
-if [ $? -eq 0 ]; then
-    echo "zshconfig success"
-else
-    echo "zshconfig failed"
-    exit -1
-fi
 
 echo "[1]if install kde  ?-------[default]"
 echo "[2]if install gnome ? "
@@ -212,6 +166,65 @@ elif [[ $REPLY =~ ^[3]$ ]]; then
     fi
 
 fi
+
+
+read -p "Normal user " username
+useradd -m -G wheel $username
+echo "$username passwd"
+passwd $username
+if [ $? -eq 0 ]; then
+    echo "user $username add success "
+else
+    echo "user $username add failed "
+    exit -1
+fi
+
+
+
+
+echo "pinyin installing"
+pacman -S fcitx fcitx-im fcitx-libpinyin --noconfirm
+echo -e "GTK_IM_MODULE DEFAULT=fcitx\nQT_IM_MODULE  DEFAULT=fcitx\nXMODIFIERS    DEFAULT=@im=fcitx " >/home/$username/.pam_environment
+chown  $username:$username /home/$username/.pam_environment
+if [ $? -eq 0 ]; then
+    echo "pinyinsuccess"
+else
+    echo "pinyinfailed"
+    exit -1
+fi
+
+
+
+echo "$usernameuser language configing"
+mkdir /home/$username/.config
+chown  $username:$username /home/$username/.config
+echo -e "LANG=zh_CN.UTF-8\nLC_CTYPE=\"zh_CN.UTF-8\"\nLC_NUMERIC=\"zh_CN.UTF-8\"\nLC_TIME=\"zh_CN.UTF-8\"\n
+LC_COLLATE=\"zh_CN.UTF-8\"\nLC_MONETARY=\"zh_CN.UTF-8\"\nLC_MESSAGES=\"zh_CN.UTF-8\"\nLC_PAPER=\"zh_CN.UTF-8\"\n
+LC_NAME=\"zh_CN.UTF-8\"\nLC_ADDRESS=\"zh_CN.UTF-8\"\nLC_TELEPHONE=\"zh_CN.UTF-8\"\nLC_MEASUREMENT=\"zh_CN.UTF-8\"\n
+LC_IDENTIFICATION=\"zh_CN.UTF-8\"\nLC_ALL= " >/home/$username/.config/locale.conf
+chown  $username:$username /home/$username/.config/locale.conf
+if [ $? -eq 0 ]; then
+    echo "$username user languageconfig success"
+else
+    echo "$username user languageconfig failed"
+    exit -1
+fi
+
+echo "zsh configing"
+pacman -S zsh oh-my-zsh-git zsh-syntax-highlighting zsh-autosuggestions autojump  --noconfirm
+chsh -s /bin/zsh $username
+ln -s /usr/share/zsh/plugins/zsh-syntax-highlighting /usr/share/oh-my-zsh/custom/plugins/
+ln -s /usr/share/zsh/plugins/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/
+cp /usr/share/oh-my-zsh/zshrc /home/$username/.zshrc
+sed 's/plugins=(git)/plugins=(autojump git zsh-syntax-highlighting zsh-autosuggestions)/g' /home/$username/.zshrc
+chown  $username:$username /home/$username/.zshrc
+if [ $? -eq 0 ]; then
+    echo "zshconfig success"
+else
+    echo "zshconfig failed"
+    exit -1
+fi
+
 
 echo "reboot enter arch quickly"
 exit 0
